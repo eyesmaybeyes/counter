ef80bce2001d837d3afea3c582ed6ee678adff4e
Maria <114153517+eyesmaybeyes@users.noreply.github.com>
2023-06-27 21:07:33 +0300

    added counter.jsx

---
+++ src/components/Counter.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "Counter.css";
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    };
+
+    return <button className="button" onClick={handleClick}>{count}</button>;
+}
+
+export default Counter;
